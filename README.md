78f005308fa173e6c722454b1adc14693f368037
Thakurdeep Singh <thakurdeep1789@gmail.com>
2026-07-15 00:02:15 -0400

    initializing

---
+++ README.md
@@ -0,0 +1,3 @@
+# 💤 Nblade_LazyVim
+
+My Lazyvim config that i use
--- plugins/flash.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "folke/flash.nvim",
-    keys = {
-      { "s", false }, -- disable in normal/visual mode
-      { "S", false }, -- disable operator-pending variant
-    },
-  },
-}
