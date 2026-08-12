9374fb31446924da2114bd4083433fc37131febd
Thakurdeep Singh <thakurdeep1789@gmail.com>
2026-08-12 11:57:54 -0400

    added stable release and started directory

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # required
+STARTDIR=$(pwd)
 sudo apt install vim cmake lazygit -y
 sudo apt remove --purge neovim-runtime neovim
 
@@ -7,13 +8,13 @@ git clone https://github.com/neovim/neovim.git /tmp/neovim
 
 cd /tmp/neovim && sudo cmake --build build/ --target uninstall
 
-git checkout nightly
+git checkout stable
 make CMAKE_BUILD_TYPE=Release
 cd build && sudo cpack -G DEB && sudo dpkg -i nvim-*.deb
 nvim -V1 -v
+cd "$STARTDIR"
 
 rm -rf /tmp/neovim
-cd -
 
 mv ~/.config/nvim{,.bak}
 
@@ -27,4 +28,4 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 rm -rf ~/.config/nvim/lua/
-ln -s $(realpath lua) ~/.config/nvim/
+ln -s $(realpath lua) ~/.config/nvim/.
